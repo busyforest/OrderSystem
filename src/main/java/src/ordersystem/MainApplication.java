3e7512008cd2a8eb6beb24d17aa0a014914505f7
BaoXu <22302010017@m.fudan.edu.cn>
2024-05-23 10:17:43 +0800

    fea:实现了商品页滚动功能

--- src/main/java/src/ordersystem/MainApplication.java
+++ src/main/java/src/ordersystem/MainApplication.java
@@ -11,7 +11,7 @@ import java.sql.SQLException;
 public class MainApplication extends Application {
     @Override
     public void start(Stage primaryStage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("primaryStage-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("userMainStage-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         primaryStage.setTitle("Order System");
         primaryStage.setScene(scene);
