3f37e98c6f0c1a7345b3c7afd1a1636cd66416e6
WangKehui <2771154500@qq.com>
2024-06-15 21:16:18 +0800

    完成大部分功能，并改进表结构

---
+++ src/main/java/src/ordersystem/entity/dish.java
@@ -0,0 +1,13 @@
+package src.ordersystem.entity;
+
+public class dish {
+    private int dishId;
+    private int sellerId;
+    private String dishName;
+    private String dishDescription;
+    private double dishPrice;
+    private String dishPictureUrl;
+    private String ingredients;
+    private String nutritionInfo;
+    private String possibleAllergens;
+}
